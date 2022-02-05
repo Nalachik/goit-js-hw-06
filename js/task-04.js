574c8c599e2d690985b17a07a919811e49563690
Maksym Kurylo <Nalachik@ukr.net>
2022-02-05 02:12:23 +0200

    task 4 begin

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,14 @@
-const counterValue = 0;
+let counterValue = 0
+const btnPlusRef = document.querySelector('button[data-action="increment"]')
+const btnMinusRef = document.querySelector('button[data-action="decrement"]')
+const valueRef = document.querySelector('#value')
+
+
+ 
+console.log(btnPlusRef)
+console.log(btnMinusRef)
+console.log(valueRef)
+// 1 - Спан +1-1
+// 4 - Добавь слушатели кликов на кнопки,
+// внутри которых увеличивай или уменьшай значение счтетчика.
+// 5 - Обновляй интерфейс новым значением переменной counterValue.
