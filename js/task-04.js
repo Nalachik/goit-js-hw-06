e3d38c7da2dd6cef9ec26d173610478282c43591
Maksym Kurylo <Nalachik@ukr.net>
2022-02-05 22:10:23 +0200

    finish tast 4
    add funtion and find first solution.

--- js/task-04.js
+++ js/task-04.js
@@ -1,13 +1,25 @@
-let counterValue = 0
+// ======== Variant 1 =======
 const btnPlusRef = document.querySelector('button[data-action="increment"]')
 const btnMinusRef = document.querySelector('button[data-action="decrement"]')
 const valueRef = document.querySelector('#value')
 
+let counterValue = 0
+function btnPlusClick() { 
+    valueRef.textContent = counterValue += 1
+}
+function btnMinusClick() { 
+    valueRef.textContent = counterValue -=1
+}
+btnPlusRef.addEventListener('click', btnPlusClick)
+btnMinusRef.addEventListener('click', btnMinusClick)
 
- 
+
 console.log(btnPlusRef)
 console.log(btnMinusRef)
 console.log(valueRef)
+
+// =========== Variant2==========
+
 // 1 - Спан +1-1
 // 4 - Добавь слушатели кликов на кнопки,
 // внутри которых увеличивай или уменьшай значение счтетчика.
