f8fc011fb845440c4d122f7634a5aaf3aa9eddea
Maksym Kurylo <Nalachik@ukr.net>
2022-02-06 21:44:06 +0200

    second solution task 9
    add refactoring/one more solution

--- js/task-09.js
+++ js/task-09.js
@@ -2,22 +2,31 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// const descriptionRef = document.querySelector('.color');
-// const buttonRef = document.querySelector('.change-color');
+         // =====   Variant 1   =====
+const descriptionRef = document.querySelector('.color');
+const buttonRef = document.querySelector('.change-color');
 
-// buttonRef.addEventListener('click', onButtonColor);
+buttonRef.addEventListener('click', onButtonColor);
 
-// function onButtonColor (event){
-//   document.body.style.backgroundColor = getRandomHexColor();
-//   descriptionRef.textContent = `${getRandomHexColor()}`;
-//  }
-    
-const btnRef = document.querySelector('.change-color');
-const textRef = document.querySelector('color');
-btnRef.addEventListener('click', ClickOnColorButton);
-ClickOnColorButton = () => {
+function onButtonColor (event){
+  document.body.style.backgroundColor = getRandomHexColor();
+  descriptionRef.textContent = `${getRandomHexColor()}`;
+ }
+           //  ===== Variant 2 ===== 
+// const btnRef = document.querySelector('.change-color');
+// const textRef = document.querySelector('.color');
 
-}
+// const ClickOnColorButton = () => {
+//   const changeColor = getRandomHexColor();
+//   textRef.textContent = changeColor;
+//   document.body.style.backgroundColor = changeColor;
+  
+// }
+// btnRef.addEventListener('click', ClickOnColorButton);
+
+// ПОЧЕМУ addEventListener в варианте 2 консоль потребовала вынести за область видимости???
+
+             // ========   TASK   ===============
 // скрипт изменяет цвет фона элемента <body> через инлайн стиль при клике
 //  на button.change-color и выводит значение цвета в span.color.
 
