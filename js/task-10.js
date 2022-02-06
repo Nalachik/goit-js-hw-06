e51be28cdd3d3b2e845746a92036204a5ca05ea1
Maksym Kurylo <Nalachik@ukr.net>
2022-02-06 23:16:57 +0200

    begin task 10
    Add selectors

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,9 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const ref = {
+  creatBtn = document.querySelector(['data-create']),  
+  destroyBtn = document.querySelector('data-destroy'),
+  controForm = document.querySelector('#controls'),
+  boxes = document.querySelector('#boxes'),
+}
