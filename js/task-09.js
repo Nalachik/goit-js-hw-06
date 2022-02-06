d61e4bb0366a1a37a39b9864140a322b67d901b1
Maksym Kurylo <Nalachik@ukr.net>
2022-02-06 21:31:50 +0200

    find one solution task 9

--- js/task-09.js
+++ js/task-09.js
@@ -2,9 +2,23 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+// const descriptionRef = document.querySelector('.color');
+// const buttonRef = document.querySelector('.change-color');
 
+// buttonRef.addEventListener('click', onButtonColor);
 
+// function onButtonColor (event){
+//   document.body.style.backgroundColor = getRandomHexColor();
+//   descriptionRef.textContent = `${getRandomHexColor()}`;
+//  }
+    
+const btnRef = document.querySelector('.change-color');
+const textRef = document.querySelector('color');
+btnRef.addEventListener('click', ClickOnColorButton);
+ClickOnColorButton = () => {
 
-// который изменяет цвета фона элемента <body> через инлайн стиль при клике
-//  на button.change - color и выводит значение цвета в span.color.
+}
+// скрипт изменяет цвет фона элемента <body> через инлайн стиль при клике
+//  на button.change-color и выводит значение цвета в span.color.
+
 // Для генерации случайного цвета используй функцию getRandomHexColor.
