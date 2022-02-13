aecf5decdbc41c5726c0e4f1fbab8c9a62083454
Maksym Kurylo <Nalachik@ukr.net>
2022-02-13 16:53:35 +0200

    update task 8
    fix console log
    add second variant, more common alert

--- js/task-08.js
+++ js/task-08.js
@@ -9,6 +9,8 @@
 //  Для доступа к элементам формы используй свойство elements.
 // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 
+    //  == Variant== 1 
+
 const form = document.querySelector('.login-form')
 
 form.addEventListener("submit", clickOnSubmitBtn);
@@ -18,16 +20,40 @@ function clickOnSubmitBtn(event) {
     
         const { email, password } =  event.currentTarget.elements
   
-    if (email.value === "") { 
-        alert("Так какое мыло говоришь?");
-    }
-    else if  (password.value === "") {
-        alert("Шо опять?");
+  if (email.value === "") {
+    alert("Так какое мыло говоришь?");
   }
-
-//   console.log(`Login: ${email.value}, Password: ${password.value}`);
+  else if (password.value === "") {
+    alert("Шо опять?");
+  }
+  else { 
+      console.log(`Login: ${email.value}, Password: ${password.value}`);
+  }
   event.currentTarget.reset();
 }
+
+//      === Variant 2===
+// const form = document.querySelector('.login-form')
+
+// form.addEventListener("submit", clickOnSubmitBtn);
+
+// function clickOnSubmitBtn(event) {
+//     event.preventDefault();
+    
+//         const { email, password } =  event.currentTarget.elements
+  
+//   if (email.value === "" || password.value === "") {
+//     alert("Please fill in all the fields below");
+//   }
+
+//   else { 
+//       console.log(`Login: ${email.value}, Password: ${password.value}`);
+//   }
+
+//   event.currentTarget.reset();
+// } 
+
+
   
 
   
